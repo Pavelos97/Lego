6addc0fc2eb040c8185acd951a18c3d7c8f2cc96
Pavel Poldiaev <Pavel.Poldiaev@yh.nackademin.se>
2018-09-28 15:02:01 +0200

    git bash test

---
+++ src/percentage/Percentage.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+package percentage;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author pavel
+ */
+class Percentage {
+
+    
+    
+    
+    }
